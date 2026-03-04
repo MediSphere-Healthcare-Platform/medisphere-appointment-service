2c76bc963a9643895a548e9e57f79600edf4406e
AroshXR <aroshanasandeep454@gmail.com>
2026-03-04 16:07:24 +0530

    initial commit

---
+++ src/main/java/com/medisphere/appointment/MedisphereAppointmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.medisphere.appointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedisphereAppointmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedisphereAppointmentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medisphere/appointment/MedisphereAppointmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medisphere.appointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedisphereAppointmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
